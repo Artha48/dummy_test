e025c9dcd93121296d1fcabd85e61ec18ad704b3
Artha48 <92628344+Artha48@users.noreply.github.com>
2026-08-27 14:31:32 +0800

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ if not firebase_admin._apps:
     cred = credentials.Certificate(kredensial_dict)
     firebase_admin.initialize_app(cred)
 
-st.success("Berhasil konek ke Firebase!")
+st.success("terkoneksi")
 
 # ============================================
 # 1. INISIALISASI FIREBASE ADMIN SDK
